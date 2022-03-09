93f125f46cb02bd450bb4d4a7b8464e99746dc19
pokhrebi <pokhrebi@dev.azure.com>
2022-03-08 19:50:22 -0500

    Add project files.

---
+++ GuitarShop/Models/ProductListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+//Bhakti Pokhrel
+namespace GuitarShop.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> Products { get; set; }
+        public string SelectedCategory { get; set; }
+        public string CheckActivityCategory(string category) => category == SelectedCategory ? "active" : "";
+    }
+}
